b2769051d159bfcd2afe122460bc0b4d4903efff
Monster <a.beltz.ab@gmail.com>
2019-03-19 18:21:19 -0500

    133

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,29 @@
+// COPY WRIGHT
+
+#include "Public/TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank"));
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController posessing : %s"), *(ControlledTank->GetName()));
+	}
+
+}
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
